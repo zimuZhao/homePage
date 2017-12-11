9115eb18c8c456b482b14683e401146be523c7a0
zhaozimu <zhaozimu@jd.com>
2017-12-11 11:18:49 +0800

    调整myWwork显示顺序;图片转化为渐进式

--- src/components/link.vue
+++ src/components/link.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="width-80 margin-auto text-align-center flex-justify-center flex-items-center flex-content-center">
     <a href="https://github.com/zimuZhao" target="_blank">
-      <span class="flex-1 ion-social-github font-size-20 textclolor-white margin-right-3"></span>
+      <span class="flex-1 ion-social-github font-size-20 textclolor-white margin-right-3"/>
     </a>
     <a href="Mailto:zhaozimu2011@hotmail.com">
-      <span class="flex-1 ion-email font-size-20 textclolor-white"></span>
+      <span class="flex-1 ion-email font-size-20 textclolor-white"/>
     </a>
   </div>
 </template>
